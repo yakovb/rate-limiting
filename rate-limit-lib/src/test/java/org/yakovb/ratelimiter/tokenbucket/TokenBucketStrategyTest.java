692d46bee1860b0bcddca438689bcb6d5b54ad4a
yakov.boglev <yakov.boglev@ezypay.com>
2020-01-20 20:54:06 +1100

    Test list for example-based tests
    for the token bucket strategy.

---
+++ rate-limit-lib/src/test/java/org/yakovb/ratelimiter/tokenbucket/TokenBucketStrategyTest.java
@@ -0,0 +1,13 @@
+package org.yakovb.ratelimiter.tokenbucket;
+
+//TODO example tests, also do a property test
+// javadoc: point is to describe the intended behaviour of the class
+public class TokenBucketStrategyTest {
+
+  //TODO null id throws
+  //TODO no previous entry creates new one
+  //TODO existing entry updates it ('it' being based on user id)
+  //TODO has sufficient tokens debits one
+  //TODO has insufficient tokens returns a limit
+  //TODO has insufficient tokens returns correct wait period
+}
