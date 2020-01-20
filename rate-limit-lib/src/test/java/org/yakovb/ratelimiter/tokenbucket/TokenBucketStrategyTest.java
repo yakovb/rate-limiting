2b50782f5a7a6c055b172bf567761834b512f24a
yakov.boglev <yakov.boglev@ezypay.com>
2020-01-20 22:25:56 +1100

    Token bucket store impl and failing test
    Add impl for token bucket data store, which is just a facade onto a
    backing Map.
    First proper failing test for the token bucket strategy as well.

--- rate-limit-lib/src/test/java/org/yakovb/ratelimiter/tokenbucket/TokenBucketStrategyTest.java
+++ rate-limit-lib/src/test/java/org/yakovb/ratelimiter/tokenbucket/TokenBucketStrategyTest.java
@@ -1,14 +1,30 @@
 package org.yakovb.ratelimiter.tokenbucket;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.Before;
 import org.junit.Test;
 import org.yakovb.ratelimiter.RequestImpl;
+import org.yakovb.ratelimiter.model.RateLimitResult;
+import org.yakovb.ratelimiter.model.Request;
+import org.yakovb.ratelimiter.model.UserRequestDataStore;
 
 //TODO example tests, also do a property test
 // javadoc: point is to describe the intended behaviour of the class
 public class TokenBucketStrategyTest {
 
-  private TokenBucketStrategy strategy = new TokenBucketStrategy(null);
+  private Map<String, TokenBucket> backingMap;
+  private TokenBucketStrategy strategy;
+
+  @Before
+  public void before() {
+    backingMap = new HashMap<>();
+    strategy = new TokenBucketStrategy(new InMemoryTokenBucketStore(backingMap));
+  }
 
   @Test(expected = IllegalArgumentException.class)
   public void emptyIdThrowsException() {
@@ -16,8 +32,22 @@ public class TokenBucketStrategyTest {
   }
 
   //TODO no previous entry creates new one
+  @Test
+  public void absentUserDataCreatesNewEntry() {
+    assertThat(backingMap).isEmpty();
+
+    Optional<RateLimitResult> result = strategy.apply(requestWithId("x"));
+
+    assertThat(result).isEmpty();
+    assertThat(backingMap.get("x")).isNotNull();
+    assertThat(backingMap).hasSize(1);
+  }
   //TODO existing entry updates it ('it' being based on user id)
   //TODO has sufficient tokens debits one
   //TODO has insufficient tokens returns a limit
   //TODO has insufficient tokens returns correct wait period
+
+  private static Request requestWithId(String id) {
+    return new RequestImpl(id, Instant.now());
+  }
 }
