ad33987ec4b2f3a8c49a1328cae8f722fab600f7
yakov.boglev <yakov.boglev@ezypay.com>
2020-01-20 21:45:23 +1100

    First simple test + pom cleanup
    Add lombok plus some test deps.
    Simple impl of request object, returning ID + time.

---
+++ rate-limit-lib/src/main/java/org/yakovb/ratelimiter/RequestImpl.java
@@ -0,0 +1,28 @@
+package org.yakovb.ratelimiter;
+
+import java.time.Instant;
+import java.util.Objects;
+import lombok.Getter;
+import org.yakovb.ratelimiter.model.Request;
+
+@Getter
+public class RequestImpl implements Request {
+
+  private final String requesterId;
+  private final Instant requestTime;
+
+  public RequestImpl(String requesterId, Instant requestTime) {
+    Objects.requireNonNull(requesterId, "Requester ID cannot be null");
+    Objects.requireNonNull(requestTime, "Request time cannot be null");
+    ensureNotEmpty(requesterId);
+
+    this.requesterId = requesterId;
+    this.requestTime = requestTime;
+  }
+
+  private void ensureNotEmpty(String requesterId) {
+    if (requesterId.trim().isEmpty()) {
+      throw new IllegalArgumentException("Requester ID cannot be empty");
+    }
+  }
+}
--- rate-limit-lib/src/test/java/org/yakovb/ratelimiter/tokenbucket/TokenBucketStrategyTest.java
+++ rate-limit-lib/src/test/java/org/yakovb/ratelimiter/tokenbucket/TokenBucketStrategyTest.java
@@ -1,10 +1,20 @@
 package org.yakovb.ratelimiter.tokenbucket;
 
+import java.time.Instant;
+import org.junit.Test;
+import org.yakovb.ratelimiter.RequestImpl;
+
 //TODO example tests, also do a property test
 // javadoc: point is to describe the intended behaviour of the class
 public class TokenBucketStrategyTest {
 
-  //TODO null id throws
+  private TokenBucketStrategy strategy = new TokenBucketStrategy();
+
+  @Test(expected = IllegalArgumentException.class)
+  public void emptyIdThrowsException() {
+    strategy.apply(new RequestImpl("", Instant.now()));
+  }
+
   //TODO no previous entry creates new one
   //TODO existing entry updates it ('it' being based on user id)
   //TODO has sufficient tokens debits one
