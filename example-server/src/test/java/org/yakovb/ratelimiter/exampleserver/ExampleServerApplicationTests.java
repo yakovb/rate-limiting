9161a610cca320eaccb5a63d7ea30081b7efc8da
yakov.boglev <yakov.boglev@ezypay.com>
2020-01-23 20:22:56 +1100

    Initial commit for example server

---
+++ example-server/src/test/java/org/yakovb/ratelimiter/exampleserver/ExampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yakovb.ratelimiter.exampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
